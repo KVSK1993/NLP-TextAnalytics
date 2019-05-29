35331358096f06ca77ba6bd16ee4ffcdfa1f5ef5
kv3singh@uwaterloo.ca <kv3singh@uwaterloo.ca>
2019-05-28 22:34:49 -0400

    Assignment 1 Commit

--- Assigment1/Assignment1.py
+++ Assigment1/Assignment1.py
@@ -1,87 +1,76 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun May 20 19:58:11 2019
+Created on Sun May 28 19:58:11 2019
 Assignment 1
 @author: Karan Vijay Singh
 Command to run script
 python Assignment1.py -p "path to data files"
 """
 
-import argparse
+#import argparse
 import re
-import pandas as pd
-import pickle
 import numpy as np
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--path", required=True, help="path to review text files")
-#ap.add_argument("-n", "--pos", required=True, help="path to pos.txt")
-
-args = vars(ap.parse_args())
-
-#open positive.txt
-open_pos_data = open(args['path']+'\\pos.txt').read()
-
-#open negative.txt
-open_neg_data = open(args['path']+'\\neg.txt').read()
-
-text,stopword_removed_text, label = [], [], []
-
-#open stopwordlist pickle file
-with open('stopwords.pickle', 'rb') as handle:
-    stopwordsList = pickle.load(handle)
-    
-#to remove empty strings/reviews.  
-positive_data = [review for review in open_pos_data.split("\n") if review]
-negative_data = [review for review in open_pos_data.split("\n") if review]
-
-#removing punctuations and stopwords from sentences
-for line in (positive_data):
-    processed_line = re.sub(r'[!"#$%&()*+\/:;<=>@[\\\]^`{|}~\t\n]', '', line)
-    label.append("1")
-    text.append(processed_line)
-    processed_without_stop_line = ' '.join([word for word in processed_line.split() if word not in stopwordsList])
-    stopword_removed_text.append(processed_without_stop_line)
-
-for line in (negative_data):
-    processed_line = re.sub(r'[!"#$%&()*+\/:;<=>@[\\\]^`{|}~\t\n]', '', line)
-    label.append("0")
-    text.append(processed_line)
-    processed_without_stop_line = ' '.join([word for word in processed_line.split() if word not in stopwordsList])
-    stopword_removed_text.append(processed_without_stop_line)
-
-# create a dataframe using texts and lables
-trainDF = pd.DataFrame()
-trainDF['text'] = text
-trainDF['stopword_removed_text'] = stopword_removed_text
-trainDF['label'] = label
-
-#for downloading the stop words from gensim library
-'''import pickle
+import sys
 import gensim
-stopwordList = gensim.parsing.preprocessing.STOPWORDS
-with open('stopwords.pickle', 'wb') as handle:
-    pickle.dump(list(stopwordList), handle, protocol=pickle.HIGHEST_PROTOCOL)'''
+import pandas as pd
+
+
+if __name__ == "__main__":
+    input_path = sys.argv[1]
+
+    stopwordList = list(gensim.parsing.preprocessing.STOPWORDS)
     
-
-#Shuffling the dataframe/dataset
-trainDF = trainDF.sample(frac=1).reset_index(drop=True)
-
-#splitting the dataset into train, validation and test
-train, validate, test = np.split(trainDF, [int(.8*len(trainDF)), int(.9*len(trainDF))])
-
-def save_to_csv(cols,df,filename):
-    df[cols].to_csv(filename, index = False)
+    #open positive.txt
+    open_pos_data = open(input_path).read()
+    open_pos_data = open_pos_data.lower()
+    text,stopword_removed_text = [], []
     
-cols_without_stopwords = ['stopword_removed_text','label']
-cols_with_stopwords = ['text','label']
-#saving with stopwords
-save_to_csv(cols_with_stopwords,train,"trainDF_with_stopwords.csv")
-save_to_csv(cols_with_stopwords,validate,"valDF_with_stopwords.csv")
-save_to_csv(cols_with_stopwords,test,"testDF_with_stopwords.csv")
-
-#saving with stopwords removed
-save_to_csv(cols_without_stopwords,train,"trainDF_without_stopwords.csv")
-save_to_csv(cols_without_stopwords,validate,"valDF_without_stopwords.csv")
-save_to_csv(cols_without_stopwords,test,"testDF_without_stopwords.csv")
-
+    #to remove empty strings/reviews.  
+    positive_data = [review for review in open_pos_data.split("\n") if review]
+    #negative_data = [review for review in open_pos_data.split("\n") if review]
+    
+    #removing punctuations and stopwords from sentences
+    for line in (positive_data):
+        processed_line = re.sub(r'[!"#$%&()*+\/:;<=>@[\\\]^`{|}~\t\n]', '', line)
+        processed_line = re.sub(r'[\']',' ',   processed_line) #remove_Apostophe    
+        processed_line = re.sub(r'[\.]',' . ',   processed_line) #replace , with ' , '
+        processed_line = re.sub(r'[\,]',' , ',   processed_line) #replace . with ' . '
+        #label.append("1")
+        text.append(processed_line.split())
+        processed_without_stop_line = [word for word in processed_line.split() if word not in stopwordList]
+        stopword_removed_text.append(processed_without_stop_line)
+    
+    # create a dataframe using texts and lables
+    #df = pd.DataFrame()
+    #df['text'] = text
+    #df['stopword_removed_text'] = stopword_removed_text
+    #trainDF['label'] = label
+    
+    
+    #splitting the dataset into train, validation and test
+    train_list, val_list, test_list = np.split(text, [int(.8*len(text)), int(.9*len(text))])
+    train_list_no_stopword, val_list_no_stopword, test_list_no_stopword = np.split(stopword_removed_text, [int(.8*len(stopword_removed_text)), int(.9*len(stopword_removed_text))])
+    
+    
+    df = pd.DataFrame(data ={"train":train_list})
+    df.to_csv("train.csv", index = False)
+    
+    df = pd.DataFrame(data ={"val":val_list})
+    df.to_csv("val.csv", index = False)
+    
+    df = pd.DataFrame(data ={"test":test_list})
+    df.to_csv("test.csv", index = False)
+    
+    df = pd.DataFrame(data ={"train":train_list_no_stopword})
+    df.to_csv("train_no_stopword.csv", index = False)
+    
+    df = pd.DataFrame(data ={"val":val_list_no_stopword})
+    df.to_csv("val_no_stopword.csv", index = False)
+    
+    df = pd.DataFrame(data ={"test":test_list_no_stopword})
+    df.to_csv("test_no_stopword.csv", index = False)
+    #
+        
+    #cols_without_stopwords = ['stopword_removed_text']
+    #cols_with_stopwords = ['text']
+    
